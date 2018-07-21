62de13797bfbaf6eda3a22f987c53538800882e2
Hoa Quach <ellick@eqhome.net>
2018-07-21 09:45:48 -0400

    animations pt 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,20 @@ import { Location } from '@angular/common';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 
+import { flyInOut, expand } from '../animations/app.animations';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+  animations: [
+      flyInOut(,
+      expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
